1e556a6d1f851ced2da5468adb1c5c630b8b7bbe
Shivangi Trivedi <116196462+Shivangi18Trivedi@users.noreply.github.com>
2022-12-01 23:51:31 +0530

    Add files via upload

---
+++ HTML_TUTORIALS.py/exercise2.py
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
+    <title>Exercise Solution</title>
+</head>
+<body>
+    <p>click here to open < href="https://googgle.co.in"
+     target=" _blank" title="google is one of the most popular search engine">google</a></p>
+    <br>
+    <a href=""./thankyou.html">Thankyou</a>
+</body>     
+</html>
---
+++ HTML_TUTORIALS.py/htmltags.py
@@ -0,0 +1,16 @@
+     <meta charset=UTF-8">
+     <meta name="viewport" content="width, initial-scale=1.0"> initial-scale=1.0">
+     <title>Document</title>
+</head>
+<body>
+     <h2> Aboout me (REAL)</H2>
+     <P>I am Harshit Vashishth working as <strong> software engineer</strong>. These days I am creating APIs 
+    using<i> flask </i> for many company project.</p>
+</body>
+</html>
+
+
+
+
+
+
